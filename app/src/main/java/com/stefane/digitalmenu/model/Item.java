64c503447cf37229ea67ca826821ddaae46c966f
Stefane Oliveira <cristinastefane.38@gmail.com>
2022-06-01 16:42:16 -0300

    Add Item

--- app/src/main/java/com/stefane/digitalmenu/model/Item.java
+++ app/src/main/java/com/stefane/digitalmenu/model/Item.java
@@ -1,4 +1,49 @@
 package com.stefane.digitalmenu.model;
 
 public class Item {
+    private int id, image;
+    private String name;
+    private float price;
+
+    public Item() {
+    }
+
+    public Item(int id, int image, String name, float price) {
+        this.id = id;
+        this.image = image;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
 }
