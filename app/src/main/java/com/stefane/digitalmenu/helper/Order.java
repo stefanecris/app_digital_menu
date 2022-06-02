7458499ae4b6f05384490001573663565ebf5e12
Stefane Oliveira <cristinastefane.38@gmail.com>
2022-06-02 17:27:43 -0300

    Add Order

--- app/src/main/java/com/stefane/digitalmenu/helper/Order.java
+++ app/src/main/java/com/stefane/digitalmenu/helper/Order.java
@@ -1,4 +1,21 @@
 package com.stefane.digitalmenu.helper;
 
 public class Order {
+
+    private int id;
+
+    public Order() {
+    }
+
+    public Order(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
