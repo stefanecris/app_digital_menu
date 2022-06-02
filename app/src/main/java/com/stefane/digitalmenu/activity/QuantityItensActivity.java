b8ab9bbef7933bb50c05949633ae12a5db3314cb
Stefane Oliveira <cristinastefane.38@gmail.com>
2022-06-01 22:49:13 -0300

    Add getItem() and displaysSelectedItem()

--- app/src/main/java/com/stefane/digitalmenu/activity/QuantityItensActivity.java
+++ app/src/main/java/com/stefane/digitalmenu/activity/QuantityItensActivity.java
@@ -3,14 +3,41 @@ package com.stefane.digitalmenu.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.stefane.digitalmenu.R;
+import com.stefane.digitalmenu.model.Item;
 
 public class QuantityItensActivity extends AppCompatActivity {
+
+    private ImageView imageCoverFood;
+    private TextView textFoodName, textFoodPrice;
+    private Item item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quantity_itens);
+
+        imageCoverFood = findViewById(R.id.imageCoverFood);
+        textFoodName = findViewById(R.id.textFoodName);
+        textFoodPrice = findViewById(R.id.textFoodPrice);
+
+        getItem();
+
+        displaysSelectedItem();
+
     }
+
+    public void getItem(){
+        item = (Item) getIntent().getSerializableExtra("Item");
+    }
+
+    public void displaysSelectedItem(){
+        imageCoverFood.setImageResource(item.getImage());
+        textFoodName.setText(item.getName());
+        textFoodPrice.setText(item.getPrice() + "");
+    }
+
 }
