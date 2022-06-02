0362368210958e14ff50b3d8e090aff20126a9b1
Stefane Oliveira <cristinastefane.38@gmail.com>
2022-06-02 14:45:57 -0300

    Add IOrderItemDAO

--- app/src/main/java/com/stefane/digitalmenu/helper/IOrderItemDAO.java
+++ app/src/main/java/com/stefane/digitalmenu/helper/IOrderItemDAO.java
@@ -1,4 +1,14 @@
 package com.stefane.digitalmenu.helper;
 
+import com.stefane.digitalmenu.model.OrderItem;
+
+import java.util.List;
+
 public interface IOrderItemDAO {
+
+    public boolean save(OrderItem orderItem);
+    public boolean update(OrderItem orderItem);
+    public boolean delete(OrderItem orderItem);
+    public List<OrderItem> list();
+
 }
