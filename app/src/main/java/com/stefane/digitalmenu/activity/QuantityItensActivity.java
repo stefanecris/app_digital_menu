e6688ec2df49cbdebe75b148c32ad5981932ced2
Stefane Oliveira <cristinastefane.38@gmail.com>
2022-06-01 21:54:38 -0300

    Add QuantityItensActivity

---
+++ app/src/main/java/com/stefane/digitalmenu/activity/QuantityItensActivity.java
@@ -0,0 +1,16 @@
+package com.stefane.digitalmenu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.stefane.digitalmenu.R;
+
+public class QuantityItensActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quantity_itens);
+    }
+}
