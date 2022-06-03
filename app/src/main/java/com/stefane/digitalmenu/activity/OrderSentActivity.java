36875a2c4094f9c92bac0f1e3978c21f320e653d
Stefane Oliveira <cristinastefane.38@gmail.com>
2022-06-03 15:33:01 -0300

    Add OrderSentActivity

--- app/src/main/java/com/stefane/digitalmenu/activity/OrderSentActivity.java
+++ app/src/main/java/com/stefane/digitalmenu/activity/OrderSentActivity.java
@@ -3,6 +3,9 @@ package com.stefane.digitalmenu.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.stefane.digitalmenu.R;
 
 public class OrderSentActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class OrderSentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_sent);
+
+        Toast.makeText(getApplicationContext(), "Pedido enviado para a cozinha com sucesso!", Toast.LENGTH_LONG).show();
+
     }
 }
