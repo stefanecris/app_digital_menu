22911ed6886bb5d0512f32636d2a24147ca9c72f
Stefane Oliveira <cristinastefane.38@gmail.com>
2022-06-03 15:32:36 -0300

    Add OrderCanceledActivity

--- app/src/main/java/com/stefane/digitalmenu/activity/OrderCanceledActivity.java
+++ app/src/main/java/com/stefane/digitalmenu/activity/OrderCanceledActivity.java
@@ -1,7 +1,11 @@
 package com.stefane.digitalmenu.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.stefane.digitalmenu.R;
 
 public class OrderCanceledActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class OrderCanceledActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_canceled);
+
+        Toast.makeText(getApplicationContext(), "Pedido cancelado", Toast.LENGTH_LONG).show();
+
     }
 }
